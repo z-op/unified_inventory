b5de18b196c5ae0ba3d679f39af309be3ee64574
SmallJoker <mk939@ymail.com>
2024-03-17 16:54:21 +0100

    Categories: remove unknown items after start
    This avoids runtime errors caused by removed items
    or such that were not registered at all but listed
    by default in a pre-defined category.

--- api.lua
+++ api.lua
@@ -184,6 +184,27 @@ minetest.after(0.01, function()
 		ui.crafts_for.recipe[outputitemname] = new_recipe_list
 	end
 
+	-- Remove unknown items from all categories
+	local total_removed = 0
+	for cat_name, cat_def in pairs(ui.registered_category_items) do
+		for itemname, exists in pairs(cat_def) do
+			if exists and not minetest.registered_items[itemname] then
+				total_removed = total_removed + 1
+				--[[
+				-- For analysis
+				minetest.log("warning", "[unified_inventory] Removed item '"
+					.. itemname .. "' from category '" .. cat_name
+					.. "'. Reason: item not registered")
+				]]
+				cat_def[itemname] = nil
+			end
+		end
+	end
+	if total_removed > 0 then
+		minetest.log("info", "[unified_inventory] Removed " .. total_removed ..
+			" unknown items from the categories.")
+	end
+
 	for _, callback in ipairs(ui.initialized_callbacks) do
 		callback()
 	end
